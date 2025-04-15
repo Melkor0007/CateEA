8c277f3292b98f22aaaecb75d01ac44487cefed6
Melkor0007 <bagualuopan@gmail.com>
2025-04-15 22:28:09 +0800

    iterative learning

---
+++ myrun_dbp15k_il_wname.sh
@@ -0,0 +1,23 @@
+CUDA_VISIBLE_DEVICES=0 python3 -u src/myrun.py \
+    --file_dir data/DBP15K/$1 \
+    --rate 0.3 \
+    --lr .0005 \
+    --epochs 1000 \
+    --hidden_units "300,300,300" \
+    --check_point 50  \
+    --bsize 512 \
+    --il \
+    --il_start 500 \
+    --semi_learn_step 5 \
+    --csls \
+    --csls_k 3 \
+    --seed 42 \
+    --tau 0.1 \
+    --tau2 4.0 \
+    --structure_encoder "gat" \
+    --img_dim 400 \
+    --attr_dim 400 \
+    --name_dim 400 \
+    --char_dim 400 \
+
+
